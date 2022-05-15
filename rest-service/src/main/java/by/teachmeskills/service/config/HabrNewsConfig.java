6751cd18d77f0ed767e98916429d1e79a584997c
Hayel <ivanforwork@yandex.ru>
2022-05-16 01:24:36 +0300

    Service configuration class added.

---
+++ rest-service/src/main/java/by/teachmeskills/service/config/HabrNewsConfig.java
@@ -0,0 +1,16 @@
+package by.teachmeskills.service.config;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HabrNewsConfig {
+    String HABR_NEWS_URL = "https://habr.com/ru/news/";
+
+    @Bean
+    public Connection connection() {
+        return Jsoup.connect(HABR_NEWS_URL);
+    }
+}
